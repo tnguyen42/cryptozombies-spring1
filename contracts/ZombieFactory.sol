a01a9aa36b0a0f83e823b5b31e0fe8a50458f15c
Thanh-Quy Nguyen <nguyen.tq@hotmail.fr>
2021-05-10 12:42:41 +0200

    :sparkles: Chapter 3 done

--- contracts/ZombieFactory.sol
+++ contracts/ZombieFactory.sol
@@ -14,6 +14,8 @@ contract ZombieFactory is Ownable {
 	uint256 dnaDigits = 16;
 	uint256 dnaModulus = 10**dnaDigits;
 
+	uint256 cooldownTime = 1 days;
+
 	string example = "MyString";
 	string example2 = "MyOtherString";
 
@@ -35,7 +37,9 @@ contract ZombieFactory is Ownable {
 	 * @param _dna The dna of the new zombie to be added
 	 */
 	function _createZombie(string memory _name, uint256 _dna) internal {
-		zombies.push(Zombie(_name, _dna, 0, 0));
+		zombies.push(
+			Zombie(_name, _dna, 1, uint32(block.timestamp + cooldownTime))
+		);
 		uint256 id = zombies.length - 1;
 		zombieToOwner[id] = msg.sender;
 		ownerZombieCount[msg.sender]++;
